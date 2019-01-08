894f1a1d061b03345452d39126cbd2c7925244bd
Noi-Git <noi.patsin@gmail.com>
2019-01-07 23:20:49 -0800

    completed the project and deploy

---
+++ gulp/tasks/build.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+browserSync = require('browser-sync').create()
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  })
+})
+
+gulp.task('deleteDistFolder', function() {
+  return del('./dist')
+})
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+  return gulp.src('./app/assets/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+    }))
+    .pipe(gulp.dest('./dist/assets/images'))
+})
+
+gulp.task('usemin', ['deleteDistFolder', 'styles'], function() {
+  return gulp.src(['./app/index.html', './app/trees.html', './app/sushi.html'])
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}]
+
+    }))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin'])
