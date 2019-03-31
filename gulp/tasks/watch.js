641c1bd96f45cb969157efd41029dc0817049437
Noi-Git <noi.patsin@gmail.com>
2019-03-31 02:28:53 -0700

    add React projects

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,9 +1,8 @@
-const gulp = require('gulp'),
-watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+const gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  browserSync = require("browser-sync").create();
 
-gulp.task('watch', function() {
-
+gulp.task("watch", function() {
   browserSync.init({
     notify: false,
     server: {
@@ -11,25 +10,19 @@ gulp.task('watch', function() {
     }
   });
 
-  watch('./app/index.html', function() {
+  watch("./app/index.html", function() {
     browserSync.reload();
   });
 
-  watch('./app/trees.html', function() {
+  watch("./app/trees.html", function() {
     browserSync.reload();
   });
 
-  watch('./app/sushi.html', function() {
-    browserSync.reload();
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("cssInject");
   });
-
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
-
 });
 
-gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/styles.css").pipe(browserSync.stream());
 });
